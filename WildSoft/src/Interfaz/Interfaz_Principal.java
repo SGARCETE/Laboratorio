f79e7349c7bf84613d6522e4faa0a3ee29968b97
Lucas.Tolosa <tolosa.lmt@gmail.com>
2015-09-25 21:11:38 -0300

    Prueba para santi

--- WildSoft/src/Interfaz/Interfaz_Principal.java
+++ WildSoft/src/Interfaz/Interfaz_Principal.java
@@ -38,6 +38,8 @@ public class Interfaz_Principal {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		System.out.println("Santi sos puto");
 	}
 
 }
