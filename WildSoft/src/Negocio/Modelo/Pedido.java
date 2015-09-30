048ff084e9b4cd02be6ad90107e91c6a69659ce3
LucasTolosa <tolosa.lmt@gmail.com>
2015-09-29 21:45:41 -0300

    Pedido modificado

--- WildSoft/src/Negocio/Modelo/Pedido.java
+++ WildSoft/src/Negocio/Modelo/Pedido.java
@@ -8,10 +8,10 @@ public class Pedido {
 	private Integer Numero_Pedido; 		// ID o Numero de pedido
 	private Date 	Fecha_Hora_Pedido;		// Fecha y hora en la que se ingresa el pedido al sistema
 	private ArrayList<Producto>	Lista_Productos;				// Lista de productos en el pedido
-	private Enum 	Estado;				// No me acuerdo como hacer el enum, Fede hacelo
+	private enum Estado_Pedido { Pendiente, Enviado, Cancelado, Preparado, Cobrado};
 	// En caso que sea delivery:
 	private Boolean Es_Delivery = false;
-	private Cliente cliente;				
+	private Cliente cliente;
 	private Date 	Hora_Entrega;			// Hora en la que se debe entregar el pedido
 	
 	/** GETTERS AND SETTERS **/
@@ -33,12 +33,6 @@ public class Pedido {
 	}
 	public void setLista_Productos(ArrayList<Producto> lista_Productos) {
 		Lista_Productos = lista_Productos;
-	}
-	public Enum getEstado() {
-		return Estado;
-	}
-	public void setEstado(Enum estado) {
-		Estado = estado;
 	}
 	public Boolean getEs_Delivery() {
 		return Es_Delivery;
