6772c2438036d9a970ac3cc7e8d99a021e09c440
NicolasCabral21 <nicolas_cabral21@hotmail.com>
2015-11-13 01:48:59 -0300

    listo contabilidad!

--- WildSoft/src/Interfaz/JDialogs/Interfaz_Contabilidad.java
+++ WildSoft/src/Interfaz/JDialogs/Interfaz_Contabilidad.java
@@ -7,6 +7,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -19,6 +20,8 @@ import com.toedter.calendar.JDateChooser;
 
 import Reportes.ReporteContabilidad;
 import java.awt.Color;
+import java.awt.Button;
+import javax.swing.JRadioButton;
 
 
 public class Interfaz_Contabilidad extends JDialog{
@@ -28,6 +31,10 @@ public class Interfaz_Contabilidad extends JDialog{
 	;
 	private JDateChooser dateChooser_2;
 	private JDateChooser dateChooser_1;
+	private	JDateChooser dateChooser ;
+	private ButtonGroup grupo1;
+	private JRadioButton rdbtnReporteDia;
+	private JRadioButton rdbtnReporteFechas ;
 
 	
 	public Interfaz_Contabilidad() {
@@ -42,18 +49,6 @@ public class Interfaz_Contabilidad extends JDialog{
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(null);
 		
-		JButton btnGenerarContabilidad = new JButton("Generar Contabilidad");
-		btnGenerarContabilidad.setBackground(Color.WHITE);
-		btnGenerarContabilidad.setHorizontalAlignment(SwingConstants.LEADING);
-		btnGenerarContabilidad.setIcon(new ImageIcon(Interfaz_Contabilidad.class.getResource("/Recursos/IMG/Product-sale-report-icon24.png")));
-		btnGenerarContabilidad.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				GenerarMiniContabilidad();
-		}
-		});
-		btnGenerarContabilidad.setBounds(502, 110, 178, 42);
-		getContentPane().add(btnGenerarContabilidad);
-		
 		JButton btnSalir = new JButton("Salir");
 		btnSalir.setBackground(Color.WHITE);
 		btnSalir.addActionListener(new ActionListener() {
@@ -67,35 +62,108 @@ public class Interfaz_Contabilidad extends JDialog{
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		panel.setBorder(new TitledBorder(null, "Selecciona Contabilidad", TitledBorder.CENTER, TitledBorder.TOP, null, null));
-		panel.setBounds(122, 22, 351, 181);
+		panel.setBounds(10, 22, 650, 224);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		Label labelPorMes = new Label("Por Mes :");
-		labelPorMes.setBounds(159, 45, 86, 22);
+		Label labelPorMes = new Label("Por Fechas :");
+		labelPorMes.setBounds(269, 126, 86, 22);
 		panel.add(labelPorMes);
 		
 		dateChooser_2 = new JDateChooser();
-		dateChooser_2.setBounds(197, 89, 130, 27);
+		dateChooser_2.setBounds(322, 154, 130, 27);
 		panel.add(dateChooser_2);
 		
 		dateChooser_1 = new JDateChooser();
-		dateChooser_1.setBounds(52, 89, 130, 27);
+		dateChooser_1.setBounds(182, 154, 130, 27);
 		panel.add(dateChooser_1);
 		
 		Label labelHasta = new Label("Hasta");
-		labelHasta.setBounds(231, 122, 62, 22);
+		labelHasta.setBounds(358, 182, 62, 22);
 		panel.add(labelHasta);
 		
 		Label labelDesde = new Label("Desde");
-		labelDesde.setBounds(97, 127, 78, 27);
+		labelDesde.setBounds(213, 182, 78, 27);
 		panel.add(labelDesde);
 		
 		// Establece la fecha de hoy por defecto
 		Calendar C = new GregorianCalendar();
 		dateChooser_1.setCalendar(C);
 		dateChooser_2.setCalendar(C);
+		
+		dateChooser = new JDateChooser();
+		dateChooser.setBounds(317, 71, 135, 32);
+		panel.add(dateChooser);
+		
+		Label lblDelDia = new Label("Del Dia");
+		lblDelDia.setBounds(250, 81, 62, 22);
+		panel.add(lblDelDia);
+		
+		JButton btnReporteDia = new JButton("Reporte Dia");
+		btnReporteDia.setBounds(462, 71, 178, 42);
+		panel.add(btnReporteDia);
+		btnReporteDia.setIcon(new ImageIcon(Interfaz_Contabilidad.class.getResource("/Recursos/IMG/Product-sale-report-icon24.png")));
+		
+		JButton btnGenerarContabilidad = new JButton("Generar Contabilidad");
+		btnGenerarContabilidad.setBounds(462, 139, 178, 42);
+		panel.add(btnGenerarContabilidad);
+		btnGenerarContabilidad.setBackground(Color.WHITE);
+		btnGenerarContabilidad.setHorizontalAlignment(SwingConstants.LEADING);
+		btnGenerarContabilidad.setIcon(new ImageIcon(Interfaz_Contabilidad.class.getResource("/Recursos/IMG/Product-sale-report-icon24.png")));
+		
+		rdbtnReporteDia = new JRadioButton("Reporte Dia");
+		rdbtnReporteDia.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dateChooser.setEnabled(true);
+				btnReporteDia.setEnabled(true);
+				dateChooser_1.setEnabled(false);
+				dateChooser_2.setEnabled(false);
+				btnGenerarContabilidad.setEnabled(false);
+			}
+		});
+		rdbtnReporteDia.setBounds(19, 71, 109, 23);
+		panel.add(rdbtnReporteDia);
+	
+		rdbtnReporteFechas = new JRadioButton("Reporte Fechas");
+		rdbtnReporteFechas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				dateChooser_1.setEnabled(true);
+				dateChooser_2.setEnabled(true);
+				btnGenerarContabilidad.setEnabled(true);
+				dateChooser.setEnabled(false);
+				btnReporteDia.setEnabled(false);
+			}
+		});
+		rdbtnReporteFechas.setBounds(19, 149, 142, 23);
+		panel.add(rdbtnReporteFechas);
+		btnGenerarContabilidad.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				GenerarMiniContabilidad();
+		}
+		});
+		
+		grupo1 = new ButtonGroup();
+		grupo1.add(rdbtnReporteDia);
+		grupo1.add(rdbtnReporteFechas);
+		
+		
+		btnReporteDia.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 GenerarMiniContabilidadDia();
+			}
+		});
+	
+	dateChooser.setEnabled(false);
+	dateChooser_1.setEnabled(false);
+	dateChooser_2.setEnabled(false);
+	btnReporteDia.setEnabled(false);
+	btnGenerarContabilidad.setEnabled(false);
+	
+	
 	}
+	
+	
 	
 	private void GenerarMiniContabilidad() {
 		if(dateChooser_1.getCalendar()!=null && dateChooser_2.getCalendar()!=null){
@@ -107,5 +175,28 @@ public class Interfaz_Contabilidad extends JDialog{
 		}
 		
 	}
+	private void GenerarMiniContabilidadDia() {
+		if(dateChooser.getCalendar()!=null){
+			Date MismaFecha1 = dateChooser.getCalendar().getTime();
+			
+			ReporteContabilidad rc = new ReporteContabilidad();
+			rc.Generar_Contabilidad(MismaFecha1, MismaFecha1);
+		}
+	
+		
+	}
+	public void verificarRadio(){
+		if(rdbtnReporteDia.isSelected())
+		{
+			dateChooser.setEnabled(true);
+			rdbtnReporteDia.setEnabled(true);
+		}
+		else if(rdbtnReporteFechas.isSelected())
+		{
+			
+		}
+		
+	}
+	
 }
 
